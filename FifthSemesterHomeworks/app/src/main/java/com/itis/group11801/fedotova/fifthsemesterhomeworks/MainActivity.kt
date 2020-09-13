76b62dea2012f5a7a943af847d24b8bb5be69ac7
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-09-13 13:59:28 +0300

    Save parameters after activity recreation

--- FifthSemesterHomeworks/app/src/main/java/com/itis/group11801/fedotova/fifthsemesterhomeworks/MainActivity.kt
+++ FifthSemesterHomeworks/app/src/main/java/com/itis/group11801/fedotova/fifthsemesterhomeworks/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.itis.group11801.fedotova.fifthsemesterhomeworks
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
@@ -7,14 +9,47 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val KEY_CALC_PREFS = "CALC_PREFS"
+        private const val KEY_RESULT_NUMBER = "RESULT_NUMBER"
+        private const val KEY_EDITED_NUMBER = "EDITED_NUMBER"
+        private const val KEY_RESULT_TEXT = "RESULT_TEXT"
+    }
+
+    private lateinit var prefs: SharedPreferences
     private var resultNumber = 0
     private var editedNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        prefs = getSharedPreferences(KEY_CALC_PREFS, Context.MODE_PRIVATE)
         setContentView(R.layout.activity_main)
         setUpChangeTheme()
         setUpCalculator()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        restorePrefs()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        savePrefs()
+    }
+
+    private fun savePrefs() {
+        prefs.edit().apply {
+            putInt(KEY_RESULT_NUMBER, resultNumber)
+            putInt(KEY_EDITED_NUMBER, editedNumber)
+            putString(KEY_RESULT_TEXT, resultTextView.text.toString())
+        }.apply()
+    }
+
+    private fun restorePrefs() {
+        resultNumber = prefs.getInt(KEY_RESULT_NUMBER, 0)
+        editedNumber = prefs.getInt(KEY_EDITED_NUMBER, 0)
+        resultTextView.text = prefs.getString(KEY_RESULT_TEXT, resources.getString(R.string.text_result))
     }
 
     private fun setUpChangeTheme() {
